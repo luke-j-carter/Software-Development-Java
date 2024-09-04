46b1dc6f944fd6809ad69639eecbf906bd7cf7a4
luke-j-carter <123020722+luke-j-carter@users.noreply.github.com>
2024-09-04 14:19:48 -0500

    Update RegisterPanel.java

--- RegisterPanel.java
+++ RegisterPanel.java
@@ -1,7 +1,9 @@
+/* DOES NOT WORK Throws a NullPointerException
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Map;
 
 public class RegisterPanel extends JPanel {
 
@@ -12,33 +14,41 @@ public class RegisterPanel extends JPanel {
 
     public RegisterPanel()
     {
+        Register register = new Register();
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.yellow);
+        panel.setPreferredSize(new Dimension(500, 500));
+
         JLabel label = new JLabel(" Enter an amount: ");
+        label.setAlignmentX(Component.LEFT_ALIGNMENT);
         this.add(label);
+
 
         //Making the box and storing user data
         box = new JTextField();
         //Editing the box
         box.setEditable(true);
         box.setPreferredSize(new Dimension(300, 30));
+        box.setAlignmentX(SwingConstants.CENTER);
         box.addActionListener(new CoinListener());
         this.add(box);
+
+        changePanel = new PursePanel();
+        this.add(changePanel);
     }
     private class CoinListener implements ActionListener
     {
         public void actionPerformed(ActionEvent e)
         {
-            String tempUserInput = box.getText();
-            double userInput = Double.parseDouble(tempUserInput);
-
-            Purse purse = register.makeChange(userInput);
-
-            changePanel.setPurse(purse);
-
+                String tempUserInput = box.getText();
+                double userInput = Double.parseDouble(tempUserInput);
+                Purse purse = register.makeChange(userInput);
         }
     }
 
 }
-
+*/
 
 
 
